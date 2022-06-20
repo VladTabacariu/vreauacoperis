33a278f2f313db7185008783945e9cfef93fdee0
VladTabacariu <airoofs.roman@gmail.com>
2022-06-20 16:55:31 +0300

    2.02

--- components/catcosta/Tigla.js
+++ components/catcosta/Tigla.js
@@ -1,4 +1,5 @@
 import { Select, NumberInput, TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import { useForceUpdate } from "@mantine/hooks";
 import { useForm } from "@mantine/form";
 import { useState, useRef } from "react";
 import jsonata from "jsonata";
@@ -10,81 +11,108 @@ const Tigla = (props) => {
     grosime: !props.oferta.tigla.grosime,
     culoare: !props.oferta.tigla.culoare,
   });
-  const modelRef = useRef(null);
-  const finisajRef = useRef(null);
-  const grosimeRef = useRef(null);
-  console.log(grosimeRef.current);
+
+  const modele = jsonata("$distinct(products[grup='tabla'].props.model)").evaluate(props);
+  const [finisaje, setFinisaje] = useState([]);
   const [grosimi, setGrosimi] = useState([]);
   const [culori, setCulori] = useState([]);
-  const modele = jsonata("$distinct(products[grup='tabla'].props.model)").evaluate(props);
-  const finisaje = jsonata("$distinct(products[grup='tabla'].props.finisaj)").evaluate(props);
+
   const form = useForm({
     initialValues: {
-      model: "CLASIC",
+      model: "",
       finisaj: "",
       grosime: "",
       culoare: "",
       pret: 0,
+      suprafata: 0,
+      total: 0,
     },
   });
+  const updateFiels = (model, finisaj, grosime, culoare) => {
+    console.log(model, finisaj, grosime, culoare);
+    const finisaje = [];
+    const grosimi = [];
+    const culori = [];
+    const pret = 0;
+    const total = 0;
+
+    finisaje = jsonata("$distinct(products[grup='tabla'].props[model='" + model + "'][].finisaj)").evaluate(props);
+    grosimi = jsonata("$distinct(products[grup='tabla'].props[(model='" + model + "') and (finisaj='" + finisaj + "')][].grosime)").evaluate(props);
+    culori = jsonata("$distinct(products[grup='tabla'].props[(model='" + model + "') and (finisaj='" + finisaj + "') and (grosime='" + grosime + "')][].culori)").evaluate(
+      props
+    );
+    pret = jsonata(
+      "products[(grup='tabla') and (props.model='" + model + "') and (props.finisaj='" + finisaj + "') and (props.grosime='" + grosime + "')].pret_lista"
+    ).evaluate(props);
+    total = form.values.suprafata * pret;
+    if (finisaje) {
+      if (!finisaje.includes(finisaj)) {
+        form.setFieldValue("finisaj", "");
+        form.setFieldValue("grosime", "");
+        form.setFieldValue("culoare", "");
+        form.setFieldValue("pret", 0);
+        form.setFieldValue("total", 0);
+      }
+      setFinisaje(finisaje);
+    }
+    if (grosimi) {
+      if (!grosimi.includes(grosime)) {
+        form.setFieldValue("grosime", "");
+        form.setFieldValue("culoare", "");
+        form.setFieldValue("pret", 0);
+        form.setFieldValue("total", 0);
+      }
+      setGrosimi(grosimi);
+    }
+    if (culori) {
+      if (!culori.includes(culoare)) {
+        form.setFieldValue("culoare", "");
+        form.setFieldValue("pret", 0);
+        form.setFieldValue("total", 0);
+      }
+      setCulori(culori);
+    }
+    if (pret) {
+      form.setFieldValue("pret", pret);
+    }
+    if (total) {
+      form.setFieldValue("total", total);
+    }
+    console.log(form.values);
+  };
   const changedModel = (value) => {
     form.setFieldValue("model", value);
+    updateFiels(value, form.values.finisaj, form.values.grosime, form.values.culoare);
     if (value) {
-      console.log(value);
-      const grosimi = jsonata("$distinct(products[grup='tabla'].props[model='" + value + "'].grosime)").evaluate(props);
       setInputsState((prevState) => ({ ...prevState, finisaj: false }));
-      setGrosimi(grosimi);
-      form.setFieldValue("finisaj", "MAT");
     }
   };
   const changedFinisaj = (value) => {
     form.setFieldValue("finisaj", value);
+    updateFiels(form.values.model, value, form.values.grosime, form.values.culoare);
     if (value) {
-      console.log(value);
       setInputsState((prevState) => ({ ...prevState, grosime: false }));
-      setGrosimi(
-        jsonata(
-          "$distinct(products[grup='tabla'].props[(model='" +
-            modelRef.current.value +
-            "') and (finisaj='" +
-            value +
-            "')][].grosime)"
-        ).evaluate(props)
-      );
     }
   };
   const changedGrosime = (value) => {
     form.setFieldValue("grosime", value);
+    updateFiels(form.values.model, form.values.finisaj, value, form.values.culoare);
     if (value) {
       setInputsState((prevState) => ({ ...prevState, culoare: false }));
-      setCulori(
-        jsonata(
-          "$distinct(products[grup='tabla'].props[(model='" +
-            modelRef.current.value +
-            "') and (finisaj='" +
-            finisajRef.current.value +
-            "') and (grosime='" +
-            value +
-            "')][].culori)"
-        ).evaluate(props)
-      );
     }
   };
   const changedCuloare = (value) => {
     form.setFieldValue("culoare", value);
+    updateFiels(form.values.model, form.values.finisaj, form.values.grosime, value);
+  };
+  const changedSuprafata = (value) => {
+    form.setFieldValue("suprafata", value);
+    form.setFieldValue("total", form.values.pret * form.values.suprafata);
   };
   return (
     <>
       <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <Select
-          label="Model"
-          description="Selectează un model"
-          placeholder="Selectează"
-          data={modele}
-          onChange={changedModel}
-          ref={modelRef}
-          value={form.values.model}
-        />
+        <Select label="Model" description="Selectează un model" placeholder="Selectează" data={modele} onChange={changedModel} value={form.values.model} />
         <Select
           label="Finisaj"
           description="Selectează finisajul"
@@ -92,7 +120,6 @@ const Tigla = (props) => {
           data={finisaje}
           onChange={changedFinisaj}
           disabled={inputsState.finisaj}
-          ref={finisajRef}
           value={form.values.finisaj}
         />
         <Select
@@ -102,7 +129,6 @@ const Tigla = (props) => {
           data={grosimi}
           onChange={changedGrosime}
           disabled={inputsState.grosime}
-          ref={grosimeRef}
           value={form.values.grosime}
         />
         <Select
@@ -114,9 +140,17 @@ const Tigla = (props) => {
           disabled={inputsState.culoare}
           value={form.values.culoare}
         />
-        <NumberInput defaultValue={0} label="Preț / metru pătrat cu TVA" placeholder="Preț" disabled />
-        <NumberInput defaultValue={0} label="Introdu cantitatea de metri pătrați" placeholder="Introdu cantitatea" />
-        <NumberInput defaultValue={0} label="Total cu TVA" placeholder="Total" disabled />
+        <NumberInput defaultValue={0} precision={2} label="Pret" description="Preț / metru pătrat cu TVA" placeholder="Preț" disabled value={form.values.pret} />
+        <NumberInput
+          defaultValue={0}
+          precision={2}
+          label="Suprafata"
+          description="Introdu cantitatea de metri pătrați"
+          placeholder="Introdu cantitatea"
+          onChange={changedSuprafata}
+          value={form.values.suprafata}
+        />
+        <NumberInput defaultValue={0} precision={2} label="Total" description="Total cu TVA" placeholder="Total" disabled value={form.values.total} />
       </form>
     </>
   );
