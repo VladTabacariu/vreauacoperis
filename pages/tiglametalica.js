1ffa76673738acd9c5e626d25440f33e4f9a5cf8
vladtabacariu <vlad.tabacariu@airoofs.ro>
2022-07-08 08:14:44 +0300

    2.1.7

--- pages/tiglametalica.js
+++ pages/tiglametalica.js
@@ -1,7 +1,7 @@
 import { Container, Select, Stepper, Text, Title, Card, Group, createStyles, Chips, Chip, SegmentedControl } from "@mantine/core";
 import { supabase } from "../utils/supabaseClient";
 import jsonata from "jsonata";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { randomId } from "@mantine/hooks";
 import Image from "next/image";
 
@@ -44,65 +44,67 @@ function Tiglametalica(props) {
       return item.props.culori[0];
     })
   );
-  console.log(culoriState);
+  products.forEach((item) => {
+    item.props.culoare = item.props.culori[0];
+  });
+  const [items, setItems] = useState(products);
+
+  const cardChange = useCallback((value, index) => {
+    setItems((prevState) =>
+      prevState.map((item, index2) => {
+        console.log(value);
+        const newItem = { ...item, props: { ...item.props, culoare: value } };
+        console.log(newItem.props.culoare);
+        console.log(newItem, index, index2);
+        return index == index2 ? newItem : item;
+      })
+    );
+  }, []);
+
   return (
     <>
       <Container>
         <Title>Tigla metalica</Title>
         <Group>
-          {products.map((item, index) => {
-            let culoare = culoriState[index];
-            return (
-              <Card key={randomId()} withBorder radius="md" p="md" className={classes.card}>
-                <Card.Section>
-                  <Image
-                    width={200}
-                    height={200}
-                    src={
-                      IMAGE_URL +
-                      item.props.model.toLowerCase() +
-                      "/" +
-                      item.props.model.toLowerCase() +
-                      "-" +
-                      item.props.finisaj.toLowerCase() +
-                      "-" +
-                      culoare +
-                      ".jpg"
-                    }
-                    alt={item.nume}
-                  />
-                </Card.Section>
-                <Card.Section className={classes.section} mt="md">
-                  <Text weight={500} size="xs">
-                    {item.nume}
-                  </Text>
-                  <Select
-                    label="Culoare"
-                    defaultValue={culoare}
-                    size="xs"
-                    data={item.props.culori}
-                    onChange={(value) => {
-                      setCuloriState((prevState) => {
-                        const newState = prevState.map((item, index2) => {
-                          if (index == index2) {
-                            return value;
-                          } else {
-                            return item;
-                          }
-                        });
-                        return newState;
-                      });
-                    }}
-                  />
-                </Card.Section>
-                <Card.Section className={classes.section} mt="md">
-                  <Text color="dimmed" size="xs">
-                    {(item.pret_lista * 5 * 1.19).toFixed(2) + " lei mp"}
-                  </Text>
-                </Card.Section>
-              </Card>
-            );
-          })}
+          {items.map((item, index) => (
+            <Card key={randomId()} withBorder radius="md" p="md" className={classes.card}>
+              <Card.Section>
+                <Image
+                  width={200}
+                  height={200}
+                  src={
+                    IMAGE_URL +
+                    item.props.model.toLowerCase() +
+                    "/" +
+                    item.props.model.toLowerCase() +
+                    "-" +
+                    item.props.finisaj.toLowerCase() +
+                    "-" +
+                    item.props.culoare +
+                    ".jpg"
+                  }
+                  alt={item.nume}
+                />
+              </Card.Section>
+              <Card.Section className={classes.section} mt="md">
+                <Text weight={500} size="xs">
+                  {item.nume}
+                </Text>
+                <Select
+                  label="Culoare"
+                  defaultValue={item.props.culoare}
+                  size="xs"
+                  data={item.props.culori}
+                  onChange={(value) => cardChange(value, index)}
+                />
+              </Card.Section>
+              <Card.Section className={classes.section} mt="md">
+                <Text color="dimmed" size="xs">
+                  {(item.pret_lista * 5 * 1.19).toFixed(2) + " lei mp"}
+                </Text>
+              </Card.Section>
+            </Card>
+          ))}
         </Group>
       </Container>
     </>
