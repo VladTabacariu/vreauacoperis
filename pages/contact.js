6c6eef309c2e45589f80aa815debdec366919493
VladTabacariu <airoofs.roman@gmail.com>
2022-07-09 11:21:18 +0300

    2.1.3

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Paper, Text, TextInput, Box, Textarea, Button, Group, SimpleGrid, createStyles } from "@mantine/core";
-import { ContactIconsList } from "../components/contacticons/ContactIcons.js";
+import { ContactIconsList } from "../components/contacticonss/ContactIcons.js";
 
 const useStyles = createStyles((theme) => {
   const BREAKPOINT = theme.fn.smallerThan("sm");
