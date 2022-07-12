694955bda6f940a4cdf85b803a7b4263a0b5fb1b
VladTabacariu <airoofs.roman@gmail.com>
2022-07-12 14:47:43 +0300

    2.3.5

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,13 @@ export default function App(props) {
         <title>vreauacoperis.ro</title>
         <meta name="description" content="Vrei sa afli cat te costa acoperisul din tigla metalica? Nimic mai simplu! Te ajutam noi!" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo/icon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo/icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo/icon/favicon-16x16.png" />
+        <link rel="manifest" href="/logo/icon/site.webmanifest" />
+        <link rel="mask-icon" href="/logo/icon/safari-pinned-tab.svg" color="#228be6" />
+        <meta name="msapplication-TileColor" content="#228be6" />
+        <meta name="theme-color" content="#228be6" />
       </Head>
 
       <MantineProvider
